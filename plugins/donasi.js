d4c621eb66f79cb13b55ccaf41d94e3686f2c6a9
naufalmuflih123 <112526698+naufalmuflih123@users.noreply.github.com>
2022-09-06 11:14:57 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,6 @@
 let handler = async m => m.reply(`
-╭─「 Donasi • Pulsa 」
-│ •  [0895-3303-79186]
-│ •  [0896-5436-0447]
+╭─「 Donasi • Pulsa/Dana 」
+│ •  [0816-1599-7326]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
